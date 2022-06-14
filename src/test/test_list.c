b78a86b6ca5374ac3f142c2a7a544869abe5ea00
deth <freemansdubz@gmail.com>
2022-06-14 19:01:10 +0300

    [fix] inserting new block in list fix

--- src/test/test_list.c
+++ src/test/test_list.c
@@ -12,6 +12,14 @@ p_pool_t pool_param = {
     .size = sizeof(pool)
 };
 
+void set_buf(void *buf, size_t size) {
+    uint8_t *ptr = buf;
+    size_t i = 0;
+    while (i < size) {
+        ptr[i++] = (uint8_t)rand(); 
+    }
+}
+
 int test_list() {
     mem_create(&m_list_desc, &pool_param);
 
@@ -22,20 +30,43 @@ int test_list() {
     void *ptr5 = mem_alloc(100);
     void *ptr6 = mem_alloc(100);
     void *ptr7 = mem_alloc(100);
-    void *ptr8 = mem_alloc(100);
-    void *ptr9 = mem_alloc(100);
-    void *ptr10 = mem_alloc(100);
+    void *ptr8 = mem_alloc(1);
+    void *ptr9 = mem_alloc(1);
+    void *ptr10 = mem_alloc(1);
+
+    set_buf(ptr1, 100);
+    set_buf(ptr2, 100);
+    set_buf(ptr3, 100);
+    set_buf(ptr4, 100);
+    set_buf(ptr5, 100);
+    set_buf(ptr6, 100);
+    set_buf(ptr7, 100);
+    set_buf(ptr8, 1);
+    set_buf(ptr9, 1);
+    set_buf(ptr10, 1);
 
     mem_free(ptr10);
     mem_free(ptr9);
     mem_free(ptr8);
+
+    ptr8 = mem_alloc(100);
+    ptr9 = mem_alloc(100);
+    ptr10 = mem_alloc(100);
+
     mem_free(ptr7);
     mem_free(ptr6);
     mem_free(ptr5);
+
+    mem_free(ptr10);
+    mem_free(ptr9);
+    mem_free(ptr8);
+
     mem_free(ptr4);
     mem_free(ptr3);
     mem_free(ptr2);
     mem_free(ptr1);
+
+
 
     return 0;
 }
